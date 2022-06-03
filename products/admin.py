863a1c670a7d665e1e486ba017586c6ad0730dec
Lawrence Lim <ll4715@nyu.edu>
2022-06-04 02:43:50 +0800

    Created second app component

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product
+from .models import Product, Test
 admin.site.register(Product)
+admin.site.register(Test)
--- products/models.py
+++ products/models.py
@@ -6,3 +6,6 @@ class Product(models.Model):
     description = models.TextField()
     price = models.TextField()
     summary = models.TextField(default='default value')
+
+class Test(models.Model):
+    thing = models.TextField(default='testsad;lfjas;ljk')
